15186fe0a3ee04ecda4fa4ccd972bfdec401b6dd
Venky <venky@anubavam.com>
2018-06-12 18:45:55 +0530

    Home page design started

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,4 +11,22 @@ export class HomePage {
 
   }
 
+  slides = [
+      {
+        title: "Welcome to the Docs!",
+        description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+        image: "assets/imgs/1_fake.jpg",
+      },
+      {
+        title: "What is Ionic?",
+        description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+        image: "assets/imgs/3_fake.jpg",
+      },
+      {
+        title: "What is Ionic Cloud?",
+        description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+        image: "assets/imgs/4_fake.jpg",
+      }
+  ];
+
 }
